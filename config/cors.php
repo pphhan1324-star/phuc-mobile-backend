afa54ac0eb23c82420b0cbccc02b54c7f8e9ab56
Phuc <pphan1324@gmail.com>
2026-06-07 17:40:10 +0700

    Update CORS for Netlify domain

--- config/cors.php
+++ config/cors.php
@@ -19,12 +19,8 @@ return [
     'allowed_methods' => ['*'],
 
     'allowed_origins' => [
-        'http://localhost:3000',
-        'http://localhost:5173',
-        'http://localhost:8000',
-        'http://127.0.0.1:3000',
-        'http://127.0.0.1:5173',
-        'http://127.0.0.1:8000',
+        'https://eclectic-chebakia-83eb9e.netlify.app',
+        '*',
     ],
 
     'allowed_origins_patterns' => [
